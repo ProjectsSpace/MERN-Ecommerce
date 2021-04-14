29ae275c8dbc670d07367c513967751814f19513
Mocarram Hosan <mocarramhosan@gmail.com>
2021-04-14 15:33:07 +0600

    First Commit

---
+++ frontend/src/data/data.js
@@ -0,0 +1,59 @@
+export const data = {
+    products: [
+        {
+            _id: 1,
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product from Nike'
+        },
+        {
+            _id: 2,
+            name: 'Nike polo Shirt',
+            category: 'T-Shirts',
+            image: '/images/p1.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product from Nike'
+        },
+        {
+            _id: 3,
+            name: 'Addidas Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 160,
+            brand: 'Addidas',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'High quality product from Addidas'
+        },
+        {
+            _id: 4,
+            name: 'Lacoste Free Pant',
+            category: 'Pants',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Lacoste',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product from Lacoste'
+        },
+        {
+            _id: 5,
+            name: 'Mars Web T-shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 600,
+            brand: 'Mars',
+            rating:5,
+            numReviews: 120,
+            description: 'Next gen t-shirt from Mars web technology'
+        },
+    ]
+}
