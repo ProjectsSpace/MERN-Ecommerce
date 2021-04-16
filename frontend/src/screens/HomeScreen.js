117fb16a1efaee08b59444530056ceb8937c6b7c
Mocarram Hosan <mocarramhosan@gmail.com>
2021-04-16 17:34:47 +0600

    Added redux functionality

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,38 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Product from "../components/Product";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
-import axios from "axios";
+import { listProducts } from "../actions/productActions";
+
+import { useDispatch, useSelector } from "react-redux";
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    // function to fetch data from API
-    const fetchData = async () => {
-      try {
-        // Setting the loading phase
-        setLoading(true);
-
-        // Fetching data from API
-        const { data } = await axios.get("/api/products");
-
-        // Turing of the loading phase
-        setLoading(false);
-
-        // Setting products data
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-
-    // Calling fetchdata function to load products
-    fetchData();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <div className="row center">
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,12 +1,13 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { data } from "./data/data";
+import { productListReducer } from "./reducer/productReducer";
 
 const initialState = {};
 
-const reducer = (state, action) => {
-  return { products: data.products };
-};
+// Getting product form reducer
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 // to show redux store in the browser devtools
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
